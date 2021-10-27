61366e5f869bdddd863be2247b70bb5e88787921
KhairiAnuar <khairianuar164@gmail.com>
2021-10-27 14:44:04 +0800

    fix broken link

--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@
       <a href="home.php"><img class="logo" src="images/logo.png" alt="Cinema Aurora Logo"></a>
   </div>
   <ul class="nav">
-      <li><a class="hvr-underline-from-center" href="home.php">Home</a></li>
+      <li><a class="hvr-underline-from-center" href="index.php">Home</a></li>
       <li><a class="hvr-underline-from-center" href="movies.php">Movies</a></li>
       <li><a class="hvr-underline-from-center" href="sessiontimes.php">Session Times</a></li>
       <li><a class="hvr-underline-from-center" href="location.php">Location</a></li>
